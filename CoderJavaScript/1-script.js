effaa7c44580e5e3067045e2076a0f77bb1eab0a
Jeremias Rodriguez <jeremias.r.n@gmail.com>
2022-04-20 22:02:06 -0300

    master

---
+++ CoderJavaScript/1-script.js
@@ -0,0 +1,12 @@
+    console.log("mensaje de prueba");
+
+    let nombre = "Jeremy"
+    console.log(nombre)
+    let textoA = "CODER"
+    let textoB = "HOUSE"
+    const SPASE = " "
+    let resultadoA = textoA + textoB;
+    alert("Hola trolaza");
+  
+    
+
---
+++ CoderJavaScript/index2.js
@@ -0,0 +1,72 @@
+/* for(let i= 0; i < 100; i++){console.log (i);}/*  */ 
+
+/* let ingresarNumero = parseInt(prompt("Ingrese un numero"));
+
+for(let i= 0; i <=10; i++){
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + " X " + i + "=" + resultado)
+} */
+
+/* for( let i = 1; i <=20; i ++){
+    let nombre= prompt("ingresar nombre")
+
+    alert("su turno es el numero " + i + " Nombre: " + nombre)
+} */
+
+/* for(let i = 0; i < 10; i ++){
+    console.log (iniciamos)
+    console.log("terminamos")
+
+    if(b)
+}
+ */
+
+/* while (<condicion>) {
+    // acá va el cuerpo del ciclo, el código que se va a repetir mientras la condición se cumpla
+  }
+   */
+
+/*  let nombre = "Jeremias"
+ let apellido = "Rodriguez"
+ let edad = "23"
+ console.log(nombre)
+ console.log(apellido)
+ console.log(edad) */
+
+/*  const ciudad1 = "buenos aires"
+ let primera = "13.400 habitantes"
+ const ciudad2 = "la pampa"
+ let segunda = "9.231 habitantes" 
+ const ciudad3 = "turkia"
+ let tercera = "133.124 habitantes"
+
+ let resultadoA = ciudad1 + primera;
+ console.log(ciudad1);
+ console.log(ciudad2)
+ console.log(ciudad3)
+ console.log(resultadoA);
+ */
+/*  console.log("mensaje de prueba")
+ let usuario = "jonh Doe"
+    console.log(usuario)
+    let productA = "Leche= 10$"
+    let consulta = prompt("¿Cual es su consulta?");
+alert(productA) */
+
+/* let entrada = prompt("Ingresar una letra");
+      let salida  = entrada + " " + "ingresada";
+      alert(salida); */
+
+      let persona    = "BART SIMPSON";
+let domicilio  = "742 EVERGREEN";
+let pais       = "USA";
+let nacimiento = "02-11-70";
+const codigo   = "B47U89RE243";
+let carnet = "Codigo: "+codigo+" "+
+             "Nombre: "+persona+" "+
+             "Pais: "+pais+" "+
+             "Nacimiento: "+nacimiento+" "+
+             "Domicilio: "+domicilio;
+console.log(carnet);
+
+
