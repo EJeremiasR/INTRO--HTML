cffc1a27226f42d36f1ba3b10fc7fcd03cba7c5f
Jeremias Rodriguez <jeremias.r.n@gmail.com>
2022-04-20 21:59:25 -0300

    Entrar

---
+++ CoderJavaScript/pito.js
@@ -0,0 +1,10 @@
+let smirnoff = "15"
+let malibu = "20"
+let licorDeMelon = "1"
+let jagger = "40"
+let vinoToroViejo = "55"
+
+let cant_smirnoff = 0
+let cant_malibu = 0
+let cant_licorDeMelon = 0
+
